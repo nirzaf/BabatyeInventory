3c9d3d8a336d4c2e43aa9924a48b51d80412aa1f
Fazrin <nirzaf@gmail.com>
2019-09-17 09:41:23 +0530

    Load DGV Updated

--- Main.cs
+++ Main.cs
@@ -130,10 +130,6 @@ namespace BabatyeInventory
             for (int i = 0; i < DGVExistingItems.Columns.Count; i = i + 2)
                 DGVExistingItems.Columns[i].DefaultCellStyle.BackColor = Color.LightGray;
 
-
-            // DGVExistingItems.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
-            //DGVExistingItems.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-
             //datagrid has calculated it's widths so we can store them
             for (int i = 0; i <= DGVExistingItems.Columns.Count - 1; i++)
             {
