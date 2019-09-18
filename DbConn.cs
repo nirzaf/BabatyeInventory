ef08e5129c8eb11d3ba92364af5c76fbc8b070c7
Fazrin <nirzaf@gmail.com>
2019-09-18 12:46:05 +0530

    "`date`"

--- DbConn.cs
+++ DbConn.cs
@@ -3,11 +3,12 @@
 namespace BabatyeInventory
 {
     class DbConn
-    {
-        //public SqlConnection Con = new SqlConnection("Data Source=CMDMFAZRIN\\SQLSERVER;Initial Catalog=babatye;Integrated Security=True; MultipleActiveResultSets=True");       
+    { 
+
         //My Local Server     
         //public SqlConnection Con = new SqlConnection("Data Source=.;Initial Catalog=babatye;Integrated Security=True; MultipleActiveResultSets=True");
         //public SqlConnection Con = new SqlConnection("Data Source=.\\SQLEXPRESS01;Initial Catalog=babatye;Integrated Security=True; MultipleActiveResultSets=True");
           public SqlConnection Con = new SqlConnection("workstation id = babatye.mssql.somee.com; packet size = 4096; user id = babatye_SQLLogin_1; pwd=2jdfhb4nco;data source = babatye.mssql.somee.com; persist security info=False;initial catalog = babatye; MultipleActiveResultSets=True");        
     }
+
 }
